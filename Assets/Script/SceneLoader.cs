c7161ca37ec44eccbc13ac7fd2e4d4d14c3456da
Joseph-Cha <joseph.c@kakao.com>
2021-03-07 11:17:46 +0900

    앱 실행 후 인게임에 들어가기 전까지 구현 코딩

---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Button StartButton;
+    public Animator Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartButton.onClick.AddListener(delegate {OnStart();});
+    }
+    
+    void OnStart()
+    {
+        Animator.SetTrigger("Start");
+        SceneManager.LoadScene("Main");
+    }
+
+
+}
