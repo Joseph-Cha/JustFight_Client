00f7202defc24a4517d03e64f64c76a0f718dd51
Joseph.c <joseph.c@kakao.com>
2021-03-09 23:19:21 +0900

    Update Assets/Script/SceneLoader.cs
    Co-authored-by: Kim Jong Feel <DeveloperFEEL@gmail.com>

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -8,7 +8,7 @@ public class SceneLoader : MonoBehaviour
 {
     public Button StartButton;
     public Animator Animator;
-
+public float loadingTime = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
