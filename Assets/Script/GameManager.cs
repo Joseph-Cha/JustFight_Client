673be84f258656dec15c01c6991ce3e850824c9d
Joseph.c <joseph.c@kakao.com>
2021-03-28 16:48:49 +0900

    Update Assets/Script/GameManager.cs
    Co-authored-by: Kim Jong Feel <DeveloperFEEL@gmail.com>

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     
     private void OnEnable()
     {
+        remainingTime = startTime;
         InvokeRepeating("CountDown", 0, 1);
     }
     private void CountDown()
