2fd72cf8426bc7735d9739bcfb77257bdd5d4236
Joseph-Cha <75019048+Joseph-Cha@users.noreply.github.com>
2021-05-02 20:38:29 +0900

    PlayerUI 구현 및 Player 구현

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,20 +5,10 @@ using UnityEngine.UI;
 using TMPro;
 public class Player : MonoBehaviour
 {
-    public TextMeshProUGUI PlayerNameText;
     public string Name { get; set; }
     private void Awake()
     {
         GetPlayerData();
-    }
-    private void Start()
-    {
-        InitPlayerSetting();
-    }
-    // Start is called before the first frame update
-    private void InitPlayerSetting()
-    {
-        PlayerNameText.text = Name;
     }
     private void GetPlayerData()
     {
